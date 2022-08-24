9f65c6b69209f4467fe2f57dc1d79c9c411021d5
Schmidt-jan <ja163sch@htwg-konstanz.de>
2022-08-25 00:04:16 +0200

    update

--- dist/wrapper.js
+++ dist/wrapper.js
@@ -1,33 +1,16 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const https_proxy_agent_1 = require("https-proxy-agent");
-const node_fetch_1 = __importDefault(require("node-fetch"));
-class NodeWrapper {
+import { HttpsProxyAgent } from "https-proxy-agent";
+import fetch from "node-fetch";
+export default class NodeWrapper {
     constructor(proxy) {
+        this.get = (url, options) => {
+            if (options) {
+                return fetch(url, { agent: this.proxyAgent });
+            }
+            return fetch(url);
+        };
         if (proxy) {
-            this.proxyAgent = new https_proxy_agent_1.HttpsProxyAgent(proxy);
+            this.proxyAgent = new HttpsProxyAgent(proxy);
         }
     }
-    get(url, options) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (options) {
-                return (0, node_fetch_1.default)(url, { agent: this.proxyAgent });
-            }
-            return (0, node_fetch_1.default)(url);
-        });
-    }
 }
-exports.default = NodeWrapper;
 //# sourceMappingURL=wrapper.js.map
